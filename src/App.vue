e29db0f4e08f21de83a678e845d605ded7bcffff
tranchien2002 <chie.tm2002@gmail.com>
2019-10-13 22:58:58 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-for="item in [1,2,3,4,5,6,7,7,8]" v-bind:key="item">
+      <Cell />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cell from "./components/Cell.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Cell
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Cell.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="cell">
+    <!-- <div class="content" v-show="isValueVisible">{{ value }}</div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cellData: Object
+    // bombIcon: {
+    //   type: String,
+    //   default: "💣"
+    // },
+    // flagIcon: {
+    //   type: String,
+    //   default: "❗"
+    // }
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    onCellLeftClicked() {
+      this.$emit("onCellLeftClicked", {
+        x: this.cellData.x,
+        y: this.cellData.y
+      });
+    },
+    onCellRightClicked() {
+      this.$emit("onCellRightClicked", {
+        x: this.cellData.x,
+        y: this.cellData.y
+      });
+    }
+  },
+  computed: {
+    value() {
+      // Revealed can be a bomb or a number
+      if (this.isRevealed) {
+        if (this.cellData.isBomb) {
+          return this.bombIcon;
+        } else {
+          return this.cellData.proximityCount > 0
+            ? this.cellData.proximityCount
+            : "";
+        }
+      }
+      // Unrevealed is marked or empty
+      return this.cellData.isMarked ? this.flagIcon : "";
+    },
+    isValueVisible() {
+      return this.isRevealed || this.cellData.isMarked;
+    },
+    isRevealed() {
+      return this.cellData.isRevealed;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.cell {
+  // table-cell causes the cells to be of equal width in the row
+  display: table-cell;
+  position: relative;
+  background-color: #eee;
+  font-weight: bold;
+  min-width: 24px;
+  max-width: 40px;
+  border: #ddd 1px solid;
+}
+
+// Equal the height to the cell's width
+.cell:before {
+  content: "";
+  display: block;
+  padding-top: 100%; /* initial ratio of 1:1*/
+}
+
+.content {
+  // Ignore padding
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  // Center content
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.revealed {
+  background-color: #fff;
+}
+</style>
